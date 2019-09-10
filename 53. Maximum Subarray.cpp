e67ff96a3ebf2a797ed49a6c7817a62853efdc74
Zh-al <54863680+Zh-al@users.noreply.github.com>
2019-09-11 00:18:00 +0300

    Update 53. Maximum Subarray.cpp

--- 53. Maximum Subarray.cpp
+++ 53. Maximum Subarray.cpp
@@ -4,21 +4,14 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int s = 0;
-        int max;
-        if (nums.at(0) < 0)
-            max = nums.at(0);
-        else
-            max = nums.at(0) * (-1);
+        int temp = nums[0];
         for (size_t i = 0; i < nums.size(); ++i)
         {
-            s = 0;
-            for (size_t j = i; j < nums.size(); ++j)
-            {
-                s += nums.at(j);
-                if (s > max)
-                    max = s;
-            }
+            s += nums[i];
+            if (s < nums[i])
+                s = nums[i];
+            temp = max (temp, s);
         }
-        return max;
+        return temp;
     }
 };
