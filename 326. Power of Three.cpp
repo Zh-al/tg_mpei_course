631345c881d329a2886f3b66aa851c12395516ef
Zh-al <54863680+Zh-al@users.noreply.github.com>
2019-09-15 23:03:26 +0300

    Create 326. Power of Three.cpp

---
+++ 326. Power of Three.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/powx-n/
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n == 0)
+            return 0;
+        while (n % 3 == 0)
+            n /= 3;
+        return (n == 1);
+    }
+};
