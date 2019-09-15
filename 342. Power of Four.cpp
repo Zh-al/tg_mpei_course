e907e58245084ab49cc60c8d489011e7572be8c4
Zh-al <54863680+Zh-al@users.noreply.github.com>
2019-09-15 23:13:05 +0300

    Create 342. Power of Four.cpp

---
+++ 342. Power of Four.cpp
@@ -0,0 +1,12 @@
+//https://leetcode.com/problems/power-of-four/submissions/
+
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        if (num == 0)
+            return 0;
+        while (num % 4 == 0)
+            num /= 4;
+        return (num == 1);
+    }
+};
