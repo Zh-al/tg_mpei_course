880218aa0bf9d5d67a1f4e17767c9a006ce3f54f
Zh-al <54863680+Zh-al@users.noreply.github.com>
2019-10-20 23:51:59 +0300

    Create 136. Single Number.cpp

---
+++ 136. Single Number.cpp
@@ -0,0 +1,58 @@
+//https://leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int sum_nums = 0;
+        int sum_set = 0;
+        set <int> n;
+        for (int i = 0; i < nums.size(); i++){
+            if (!n.count(nums[i])){
+                n.insert (nums[i]);
+                sum_set += nums[i];
+            }
+            sum_nums += nums[i];
+        }
+        return 2 * sum_set - sum_nums;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        int ret = Solution().singleNumber(nums);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
