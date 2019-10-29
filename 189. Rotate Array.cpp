e9145945941ffb78d2dc6d9a6be900cbc4b6a578
Zh-al <54863680+Zh-al@users.noreply.github.com>
2019-10-29 23:55:14 +0300

    Create 189. Rotate Array.cpp

---
+++ 189. Rotate Array.cpp
@@ -0,0 +1,75 @@
+//https://leetcode.com/problems/rotate-array/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if (nums.size() == 0)
+            return;
+        k = k % nums.size();
+        nums.insert (nums.begin(), nums.end() - k, nums.end());
+        nums.erase (nums.end() - k, nums.end());
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        getline(cin, line);
+        int k = stringToInteger(line);
+        
+        Solution().rotate(nums, k);
+
+        string out = integerVectorToString(nums);
+        cout << out << endl;
+    }
+    return 0;
+}
