596c13c4a0b64b8a63ac3af52aede0e950d8ee1d
Zh-al <54863680+Zh-al@users.noreply.github.com>
2019-11-03 14:30:11 +0300

    Create 258. Add Digits.cpp

---
+++ 258. Add Digits.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/add-digits/
+
+class Solution {
+public:
+    int addDigits(int num) {
+        return 1 + (num - 1) % 9;
+    }
+};
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        int num = stringToInteger(line);
+        
+        int ret = Solution().addDigits(num);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
