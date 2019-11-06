01cc660f2c0413271847577c3f5c516a1c7af6c0
Zh-al <54863680+Zh-al@users.noreply.github.com>
2019-11-06 23:17:08 +0300

    Create 350. Intersection of Two Arrays II.cpp

---
+++ 350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,84 @@
+//https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size())
+            return intersect (nums2, nums1);
+        unordered_multiset <int> nums;
+        //unordered_set <int> res;
+        vector <int> result;
+        for (int i = 0; i < nums1.size(); i++)
+            nums.insert(nums1[i]);
+        for (int i = 0; i < nums2.size(); i++){
+            const auto it = nums.find (nums2[i]);
+            if (it != nums.end()){
+                result.push_back(nums2[i]);
+                nums.erase(it);
+            }
+        }
+        /*for (auto i : res)
+            result.push_back (i);
+            */
+        return result;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums1 = stringToIntegerVector(line);
+        getline(cin, line);
+        vector<int> nums2 = stringToIntegerVector(line);
+        
+        vector<int> ret = Solution().intersect(nums1, nums2);
+
+        string out = integerVectorToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
