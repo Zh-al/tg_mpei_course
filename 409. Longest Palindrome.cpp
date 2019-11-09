404cd66b4f9a0f6d53376c5e02971ab646eacf69
Zh-al <54863680+Zh-al@users.noreply.github.com>
2019-11-09 21:13:43 +0300

    Create 409. Longest Palindrome.cpp

---
+++ 409. Longest Palindrome.cpp
@@ -0,0 +1,58 @@
+//https://leetcode.com/problems/longest-palindrome/
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        map <char, int> let;
+        bool odd = false;
+        int res = 0;
+        for (int i = 0; i < s.length(); i++)
+            let[s[i]]++;
+        for (auto i : let){
+            res += i.second / 2;
+            if (i.second & 1)
+                odd = true;
+        }
+        res *= 2;
+        return odd ? res + 1 : res;
+    }
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string s = stringToString(line);
+        
+        int ret = Solution().longestPalindrome(s);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
