3cd195c3e5b91bc695afc49b21312f72e2fac6b1
Zh-al <54863680+Zh-al@users.noreply.github.com>
2019-11-11 14:10:10 +0300

    Create 507. Perfect Number.cpp

---
+++ 507. Perfect Number.cpp
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/perfect-number/
+
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+
+            }
+        }
+        return sum - num == num;
+    }
+};
+
+int stringToInteger(string input) {
+    return stoi(input);
+}
+
+string boolToString(bool input) {
+    return input ? "True" : "False";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        int num = stringToInteger(line);
+        
+        bool ret = Solution().checkPerfectNumber(num);
+
+        string out = boolToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
