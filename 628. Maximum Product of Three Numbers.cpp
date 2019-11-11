35f571dc8ae6e9684fbe4c9a9d651b1557d19591
Zh-al <54863680+Zh-al@users.noreply.github.com>
2019-11-11 19:06:18 +0300

    Create 628. Maximum Product of Three Numbers.cpp

---
+++ 628. Maximum Product of Three Numbers.cpp
@@ -0,0 +1,50 @@
+//https://leetcode.com/problems/maximum-product-of-three-numbers/
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return max (nums[0] * nums[1] * nums[nums.size() - 1], 
+                    nums[nums.size() - 1] * nums[nums.size() - 2] *nums[nums.size() - 3]);
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        int ret = Solution().maximumProduct(nums);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
