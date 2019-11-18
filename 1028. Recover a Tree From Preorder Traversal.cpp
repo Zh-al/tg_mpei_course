b120273acdc16b08fdd2470058455e817cda14db
Zh-al <54863680+Zh-al@users.noreply.github.com>
2019-11-18 22:00:39 +0300

    Create 1028. Recover a Tree From Preorder Traversal.cpp

---
+++ 1028. Recover a Tree From Preorder Traversal.cpp
@@ -0,0 +1,98 @@
+//https://leetcode.com/problems/recover-a-tree-from-preorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* recoverFromPreorder(string S) {
+        int i = 0;
+        return f(S, i, 0);
+    }
+    
+    TreeNode* f(string& s, int& i, int n){
+        int j = 0;
+        while (s[i + j] == '-') ++j;
+        if (j != n) return NULL;
+        i += j;
+        int k = 0;
+        while (isdigit(s[i])) {
+            k = k * 10 + s[i] - '0';
+            ++i;
+        }
+        TreeNode* r = new TreeNode(k);
+        r->left = f(s, i, n+1);
+        r->right = f(s, i, n+1);
+        return r;
+    }
+    
+};
+
+string stringToString(string input) {
+    assert(input.length() >= 2);
+    string result;
+    for (int i = 1; i < input.length() -1; i++) {
+        char currentChar = input[i];
+        if (input[i] == '\\') {
+            char nextChar = input[i+1];
+            switch (nextChar) {
+                case '\"': result.push_back('\"'); break;
+                case '/' : result.push_back('/'); break;
+                case '\\': result.push_back('\\'); break;
+                case 'b' : result.push_back('\b'); break;
+                case 'f' : result.push_back('\f'); break;
+                case 'r' : result.push_back('\r'); break;
+                case 'n' : result.push_back('\n'); break;
+                case 't' : result.push_back('\t'); break;
+                default: break;
+            }
+            i++;
+        } else {
+          result.push_back(currentChar);
+        }
+    }
+    return result;
+}
+
+string treeNodeToString(TreeNode* root) {
+    if (root == nullptr) {
+      return "[]";
+    }
+
+    string output = "";
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()) {
+        TreeNode* node = q.front();
+        q.pop();
+
+        if (node == nullptr) {
+          output += "null, ";
+          continue;
+        }
+
+        output += to_string(node->val) + ", ";
+        q.push(node->left);
+        q.push(node->right);
+    }
+    return "[" + output.substr(0, output.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        string S = stringToString(line);
+        
+        TreeNode* ret = Solution().recoverFromPreorder(S);
+
+        string out = treeNodeToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
